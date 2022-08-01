a9fd88595991341d12117b602fc2de1b94f7097c
Dominik Kriese <dominik.kriese@novatec-gmbh.de>
2022-08-01 21:04:45 +0200

    NGRX: Create app with basic testing examples

---
+++ apps/ngrx/src/app/routes.ts
@@ -0,0 +1,9 @@
+import {WeatherPageComponent} from './pages/weather/weather-page.component';
+import {EntryComponent} from './pages/entry/entry.component';
+import {DefinedRoute} from '@cntws/layout';
+
+
+export const DEFINED_ROUTES: DefinedRoute[] = [
+  new DefinedRoute('weather', WeatherPageComponent, 'Weather'),
+  new DefinedRoute('**', EntryComponent, 'Home'),
+];
