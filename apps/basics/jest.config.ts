de26f07852d4c0b09b487fbc46f45c64c52e0839
Dominik Kriese <extern.kriese_dominik@allianz.de>
2024-04-09 09:06:13 +0200

    NG17: Migrate to nx 17

--- apps/basics/jest.config.ts
+++ apps/basics/jest.config.ts
@@ -5,15 +5,16 @@ export default {
   displayName: 'basics',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../coverage/apps/basics',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   globalSetup: 'jest-preset-angular/global-setup',
   transformIgnorePatterns: [`<rootDir>/node_modules/(?!.*\\.mjs$|${esModules.join('|')})`],
